fe2f9fc2130c3ae3cb6fd6eea37a522ffe65b667
nlehuythai <nlht081005@gmail.com>
2026-05-30 22:23:46 +0700

    feat: hoàn thiện kết nối database neon

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -41,7 +41,7 @@ app.use('/api/admin', adminRoutes);
 
 const doctorRoutes = require('./routes/doctorRoutes');
 app.use('/api/doctor', doctorRoutes);
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server đang chạy trên cổng ${PORT}`);
